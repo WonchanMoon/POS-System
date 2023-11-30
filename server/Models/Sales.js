582e89e2fe743fce1e8189a621c8e11d908b2759
hlee225 <152259496+hlee225@users.noreply.github.com>
2023-11-30 19:52:15 +0900

    Add files via upload

---
+++ server/Models/Sales.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'); // mongoose 선언
+
+const salesSchema = mongoose.Schema({  // schema 작성
+  ID: {
+    type: String,
+    maxLength: 10,
+    trim: true, // space 없애줌
+    unique: 1, // 유일
+  },
+  name: {
+    type: String,
+    maxLength: 20,
+  },
+  price: {
+    type: Number,
+    default: 0, // 값이 정해지지 않았다면 디폴트 0
+  },
+  counts: {
+    type: Number,
+    default: 0, // 값이 정해지지 않았다면 디폴트 0
+  },
+  date: {
+    type: String,
+    maxLength: 50,
+  }
+});
+
+const Sales = mongoose.model('Sales', salesSchema); // Schema를 model로 감싸줌
+
+module.exports = { Sales }; // Product라는 모델을 본 파일 밖에서도 사용할 수 있도록 export 구문을 작성해줌
+
+//curl -X POST -H "Content-Type: application/json" -d '{"ID":"1234", "name": "고기", "price": 5000, "counts":3}' http://localhost:8000/products
