54aee3300b2465b5894e9699774c5d663d774b69
2019008813 <0001wonchan@naver.com>
2023-11-30 23:00:59 +0900

    business 할인 적용

--- server/Models/Product.js
+++ server/Models/Product.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose'); // mongoose 선언
 const productSchema = mongoose.Schema({  // schema 작성
   ID: {
     type: String,
-    maxLength: 10,
     trim: true, // space 없애줌
     unique: 1, // 유일
     // required: true,
