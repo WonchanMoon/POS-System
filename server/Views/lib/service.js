8f36f34a35530709f53937677f7530b96dc4bd19
JMKiim <jmkacc99@gmail.com>
2023-12-01 20:43:44 +0900

    service rollback

--- server/Views/lib/service.js
+++ server/Views/lib/service.js
@@ -278,27 +278,27 @@ function clearReceivedAmount() {
 
 // 상품을 로드하고 표시하는 함수
 function loadProducts() {
-  // var products = [
-  //   { name: '상품 A', price: 1000, discount: 10 },
-  //   { name: '상품 B', price: 1500, discount: 5 },
-  //   { name: '상품 C', price: 2000, discount: 0 },
-  //   { name: '상품 D', price: 2000, discount: 0 },
-  //   { name: '상품 E', price: 2000, discount: 0 },
-  //   { name: '상품 F', price: 2000, discount: 0 },
-  //   { name: '상품 G', price: 2000, discount: 0 },
-  //   { name: '상품 H', price: 2000, discount: 0 },
-  // ];
+  var products = [
+    { name: '상품 A', price: 1000, discount: 10 },
+    { name: '상품 B', price: 1500, discount: 5 },
+    { name: '상품 C', price: 2000, discount: 0 },
+    { name: '상품 D', price: 2000, discount: 0 },
+    { name: '상품 E', price: 2000, discount: 0 },
+    { name: '상품 F', price: 2000, discount: 0 },
+    { name: '상품 G', price: 2000, discount: 0 },
+    { name: '상품 H', price: 2000, discount: 0 },
+  ];
 
-  var products = [];
-  $.ajax({
-    type: 'GET',
-    url: '/products',
-    data: {},
-    dataType: 'json',
-  }).done((result) => {
-    console.log(result);
-    products = result;
-  });
+  // var products = [];
+  // $.ajax({
+  //   type: 'GET',
+  //   url: '/products',
+  //   data: {},
+  //   dataType: 'json',
+  // }).done((result) => {
+  //   console.log(result);
+  //   products = result;
+  // });
 
   var productCatalog = document.querySelector('.product-catalog');
   products.forEach(function (product) {
