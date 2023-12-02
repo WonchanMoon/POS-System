89b49eff828b701e4e56580cdae143a507a72434
JMKiim <jmkacc99@gmail.com>
2023-12-02 22:00:24 +0900

    service lowCounts fix

--- server/Views/lib/service.js
+++ server/Views/lib/service.js
@@ -376,7 +376,7 @@ function lowCounts(product) {
     url: '/products/counts/' + product.name,
     type: 'PUT',
     contentType: 'application/json',
-    data: JSON.stringify({ productName: product.name, counts: product.counts }),
+    data: JSON.stringify({ name: product.name, counts: product.counts }),
     success: function (response) {
       console.log('successful:', response);
     },
